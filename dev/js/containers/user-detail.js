e8d168cff3f4c7e67e6b6a905299bc5c61674524
anirvan90 <anirvan.awatramani@gmail.com>
2017-06-14 17:36:27 -0700

    basic app working

--- dev/js/containers/user-detail.js
+++ dev/js/containers/user-detail.js
@@ -1,31 +1,25 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-
-/*
- * We need "if(!this.props.user)" because we set state to null by default
- * */
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
 class UserDetail extends Component {
-    render() {
-        if (!this.props.user) {
-            return (<div>Select a user...</div>);
-        }
-        return (
-            <div>
-                <img src={this.props.user.thumbnail} />
-                <h2>{this.props.user.first} {this.props.user.last}</h2>
-                <h3>Age: {this.props.user.age}</h3>
-                <h3>Description: {this.props.user.description}</h3>
-            </div>
-        );
+  render () {
+    if (!this.props.user) {
+      return (<h4>Select A User...</h4>)
     }
+    return (
+      <div>
+        <h2>Name: {this.props.user.first} {this.props.user.last}</h2>
+        <h2>Description: {this.props.user.description}</h2>
+        <h3>Age: {this.props.user.age}</h3>
+      </div>
+    )
+  }
 }
 
-// "state.activeUser" is set in reducers/index.js
-function mapStateToProps(state) {
-    return {
-        user: state.activeUser
-    };
+function mapStateToProps (state) {
+  return {
+    user: state.activeUser
+  }
 }
 
-export default connect(mapStateToProps)(UserDetail);
+export default connect(mapStateToProps)(UserDetail)
--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,33 +1,32 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
-
 export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
-        }
-    ]
+  return [
+    {
+      id: 1,
+      first: 'Anirvan',
+      last: 'Awatramani',
+      age: 26,
+      description: 'Full Stack Engineer'
+    },
+    {
+      id: 2,
+      first: 'Dhruv',
+      last: 'Bhalla',
+      age: 25,
+      description: 'Hardware/Materials Engineer'
+    },
+    {
+      id: 3,
+      first: 'Varun',
+      last: 'Anand',
+      age: 26,
+      description: 'Strategy Lead / Chief Consultor'
+    },
+    {
+      id: 4,
+      first: 'Bobby',
+      last: 'Shah',
+      age: 27,
+      description: 'Head of Security'
+    }
+  ]
 }
